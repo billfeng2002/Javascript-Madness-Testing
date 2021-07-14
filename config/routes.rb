e09261c1bd14cdf0a27e4fa795ac413dbe04ee6d
Lindsay <lindsay.chen@yale.edu>
2021-07-15 04:14:57 +0800

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :concerts
+  resources :concert_halls
+  resources :repertoires
+  resources :orchestras
 end
