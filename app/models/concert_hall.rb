9165d9df00e4e5465094041c46dbc6d4c07738d1
Lindsay <lindsay.chen@yale.edu>
2021-07-16 14:54:01 +0800

    concert hall show page

--- app/models/concert_hall.rb
+++ app/models/concert_hall.rb
@@ -2,4 +2,12 @@ class ConcertHall < ApplicationRecord
     has_many :concerts
     has_many :orchestras, through: :concerts
     has_many :users, through: :concerts
+
+    def past_concerts
+        Concert.past_concerts.select{|concert| concert.concert_hall == self}
+    end
+
+    def future_concerts
+        Concert.future_concerts.select{|concert| concert.concert_hall == self}
+    end
 end
