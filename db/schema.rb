beb99235435434336c5896a90342a2bda733b659
Bill <billfeng2002@gmail.com>
2021-07-20 15:08:45 -0700

    don e

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_15_185302) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "concert_halls", force: :cascade do |t|
     t.string "location"
     t.integer "date_built"
